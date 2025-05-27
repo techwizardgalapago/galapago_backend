b4f535b47d9506b1fa2b17d996a52e4a5952a8c8
techwizardgalapago <techwizard.galapago@gmail.com>
2025-05-27 09:02:35 -0600

    probamos cosas para que funcione redis

--- libs/redis.client.js
+++ libs/redis.client.js
@@ -5,40 +5,55 @@ console.log("Redis URL: ", config.redis.url);
 
 const DEFAULT_EXPIRATION = 30;
 
-let redisClient = null;
+// let redisClient = null;
 
-async function initializeRedisClient() {
-  if (!redisClient) {
-    redisClient = new Redis.Cluster(
-      [
-        {
-          host: config.redis.host,
-          port: config.redis.port,
-        },
-      ],
-      {
-        dnsLookup: (address, callback) => callback(null, address),
-        redisOptions: {
-          tls: {},
-        },
-      }
-    );
+// async function initializeRedisClient() {
+//   if (!redisClient) {
+//     redisClient = new Redis.Cluster(
+//       [
+//         {
+//           host: config.redis.host,
+//           port: config.redis.port,
+//         },
+//       ],
+//       {
+//         dnsLookup: (address, callback) => callback(null, address),
+//         redisOptions: {
+//           tls: {},
+//         },
+//       }
+//     );
 
-    redisClient.on("error", (err) => console.log("Redis Client Error", err));
-    redisClient.on("connect", () => {
-      console.log("Redis client connected");
-    });
-    redisClient.on("ready", () => {
-      console.log("Redis client is ready");
-    });
-    await redisClient.connect();
+//     redisClient.on("error", (err) => console.log("Redis Client Error", err));
+//     redisClient.on("connect", () => {
+//       console.log("Redis client connected");
+//     });
+//     redisClient.on("ready", () => {
+//       console.log("Redis client is ready");
+//     });
+//     await redisClient.connect();
+//   }
+//   return redisClient;
+// }
+
+const redisClient = new Redis.Cluster(
+  [
+    {
+      host: config.redis.host,
+      port: config.redis.port,
+    },
+  ],
+  {
+    dnsLookup: (address, callback) => callback(null, address),
+    redisOptions: {
+      tls: {},
+    },
   }
-  return redisClient;
-}
+);
 
 async function getOrSetCache(key, cb) {
   try {
-    initializeRedisClient();
+    // initializeRedisClient();
 
     const data = await redisClient.get(key);
     if (data != null) {
